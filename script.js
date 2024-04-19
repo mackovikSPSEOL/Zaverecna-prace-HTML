1f3bea7c59482e3ce5ee98efc980d28d334eae88
Mates Mackovík <mac42396@spseol.cz>
2024-04-19 21:07:02 +0200

    prvni commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+
+}
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+
+}
